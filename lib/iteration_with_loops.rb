e710f2b1f1082d1801b545649e420b301dbfce5e
yehudis raitman <yehudisr@gmail.com>
2021-01-18 23:28:36 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,7 +9,7 @@ while row_index < src.count do
   
   element_index = 0 
   while element_index < src[row_index].count do
-    if src[row_index][element_index].even
+    if src[row_index][element_index].even?
       p src[row_index][element_index]
     end
     element_index += 1 
